429cb7d92ce67159cd18a772214e7d7c31761f5e
Thomas Lübking <thomas.luebking@gmail.com>
2026-09-11 08:10:28 +0200

    make the shell interfacing dbus methods return the pty path
    instead of the inaccessible/redundant QDBusObjectPath
    
    part of #1374

--- src/termwidget.h
+++ src/termwidget.h
@@ -87,13 +87,13 @@ class TermWidget : public QWidget, public DBusAddressable
         void propertiesChanged();
         QStringList availableKeyBindings() { return m_term->availableKeyBindings(); }
 
-        TermWidgetImpl * impl() { return m_term; }
+        TermWidgetImpl * impl() const { return m_term; }
 
         #ifdef HAVE_QDBUS
         QDBusObjectPath splitHorizontal(const QHash<QString,QVariant> &termArgs);
         QDBusObjectPath splitVertical(const QHash<QString,QVariant> &termArgs);
-        QDBusObjectPath splitHorizontal(const QString &dbus_id, const QString &shell_command, const QString &workdir, const int newPercent);
-        QDBusObjectPath splitVertical(const QString &dbus_id, const QString &shell_command, const QString &workdir, const int newPercent);
+        QString splitHorizontal(const QString &dbus_id, const QString &shell_command, const QString &workdir, const int newPercent);
+        QString splitVertical(const QString &dbus_id, const QString &shell_command, const QString &workdir, const int newPercent);
         QDBusObjectPath getTab();
         void sendText(const QString& text);
         void closeTerminal();
@@ -101,6 +101,7 @@ class TermWidget : public QWidget, public DBusAddressable
         void setBackgroundImage(const QString &image, const int mode);
         void setFont(const QString& font, const int pointSize);
         void setSize(int cloumns, int lines);
+        QString ptyPath() const;
         #endif
 
         bool eventFilter(QObject * obj, QEvent * evt) override;
